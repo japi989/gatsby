65377d6445e7f65383b2f0cb9ea6784dc14c3907
Bojan <japi989@hotmail.com>
2022-01-18 20:44:01 +0100

    first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,61 @@ module.exports = {
   plugins: [{
     resolve: 'gatsby-source-wordpress',
     options: {
-      "url": " http://learning-gatsby.test/graphql"
+      "url": "http://learning-gatsby.test/graphql"
     }
-  }, "gatsby-plugin-sass"]
+  },
+  "gatsby-plugin-sass",
+  `gatsby-plugin-image`,
+  `gatsby-plugin-sharp`,
+  `gatsby-transformer-sharp`,
+  {
+    resolve: 'gatsby-plugin-flexsearch',
+    options: {
+      languages: ['en'],
+      type: 'MarkdownRemark',
+      fields: [
+        {
+          name: 'title',
+          indexed: true,
+          resolver: 'frontmatter.title',
+          attributes: {
+            encode: 'balance',
+            tokenize: 'strict',
+            threshold: 6,
+            depth: 3,
+          },
+          store: true,
+        },
+        {
+          name: 'description',
+          indexed: true,
+          resolver: 'frontmatter.description',
+          attributes: {
+            encode: 'balance',
+            tokenize: 'strict',
+            threshold: 6,
+            depth: 3,
+          },
+          store: false,
+        },
+        {
+          name: 'url',
+          indexed: false,
+          resolver: 'fields.slug',
+          store: true,
+        },
+      ],
+    },
+  },{
+      resolve: "gatsby-source-graphql",
+      options: {
+        // Remote schema query type. This is an arbitrary name.
+        typeName: "WPGraphQL",
+        // Field name under which it will be available. Used in your Gatsby query. This is also an arbitrary name.
+        fieldName: "wpcontent",
+        // GraphQL endpoint, relative to your WordPress home URL.
+        url: "http://learning-gatsby.test/graphql",
+      },
+    },
+  ]
 };
